60182783a4813fac48c3e574f66c7668c1f4fc5e
xingjian-zhang <jimmyzxj@umich.edu>
2021-12-17 17:14:47 -0500

    arch

--- arch.py
+++ arch.py
@@ -1,13 +1,15 @@
 import torch
+import numpy as np
 from torchsummary import summary
 
 from config import IMAGE_HEIGHT, IMAGE_WIDTH
 from model import UNET
 
-layers = [1, 2, 3, 4, 5]
-features = [64, 128, 256, 512, 1024]
-for l in layers:
-    print("LAYERS = ", l)
-    u = UNET(3, 1, features=features[:l]).cuda()
+scale = [1, 2, 4, 8, 16]
+features = [64, 128, 256, 512]
+features = np.array(features)
+for l in scale:
+    print("channels = ", features[0]//l)
+    u = UNET(3, 1, features=features//l).cuda()
     summary(u, (3, 256, 256))
     print("\n")
